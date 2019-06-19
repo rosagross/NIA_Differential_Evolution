851b893bd80da24c80628d156333c5619b204ffd
earens <49277057+earens@users.noreply.github.com>
2019-06-19 15:09:14 +0200

    Update DifferentialEvolution.java

--- DifferentialEvolution.java
+++ DifferentialEvolution.java
@@ -2,4 +2,16 @@ package differentialEvolution;
 
 public class DiffenrentialEvolution {
 
+	private int popSize;
+	double[][] population;
+	
+	public DiffenrentialEvolution(int popSize) {
+		
+		this.popSize = popSize;
+	}
+	
+	Initialization init = new Initialization(popSize);
+	population = init.initialize();
+
+	
 }
