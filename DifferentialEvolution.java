8fdec5225d7163a8937a4d72474848e0f2661b22
earens <49277057+earens@users.noreply.github.com>
2019-06-19 15:56:09 +0200

    Update DifferentialEvolution.java

--- DifferentialEvolution.java
+++ DifferentialEvolution.java
@@ -1,17 +1,53 @@
 package differentialEvolution;
 
-public class DiffenrentialEvolution {
+public class DifferentialEvolution {
 
-	private int popSize;
-	double[][] population;
+	private Market[] markets;
+	private Plant[] plants;
 	
-	public DiffenrentialEvolution(int popSize) {
+	private int popSize;
+	private double scaleFactor;
+	private double crossoverRate;
+	
+	private double[][] population;
+	private double[][] newPopulation;
+	
+	private double[] donor;
+	private double[] trial;
+
+	
+	final public int DIMENSIONS = 9;
+	
+	public DifferentialEvolution(Plant[] plants, Market[] markets, int popSize, double scaleFactor, double crossoverRate) {
 		
+		this.plants = plants;
+		this.markets = markets;
 		this.popSize = popSize;
+		this.scaleFactor = scaleFactor;
+		this.crossoverRate = crossoverRate;
 	}
 	
-	Initialization init = new Initialization(popSize);
-	population = init.initialize();
+	
+	public double[][] differentialEvolution() {
+		
+		
+		Initialization init = new Initialization(popSize);
+		population = init.initialize();
+		newPopulation = new double[popSize][DIMENSIONS];
+		DonorGeneration donorGen = new DonorGeneration(scaleFactor);
+		TrialGeneration trialGen = new TrialGeneration(crossoverRate);
+		Selection select = new Selection();
+
+		
+		for (int i = 0; i < popSize; i++) {
+			donor = donorGen.generateDonor(population);
+			trial = trialGen.generateTrial(donor, population[i]);
+			newPopulation[i] = select.select(trial, population[i]);
+		}
+		
+
+	}
+	
 
 	
 }
