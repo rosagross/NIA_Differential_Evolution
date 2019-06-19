a43607639d059d0b65b30ac479ae049b3aaf05db
earens <49277057+earens@users.noreply.github.com>
2019-06-19 14:18:53 +0200

    Update Market.java

--- Market.java
+++ Market.java
@@ -12,4 +12,25 @@ public class Market {
 		this.price = price;
 		this.demand = demand;
 	}
+	
+	
+	
+	private double demand(double price, double maxPrice, double maxDemand) {
+		
+		//if price is greater than max price, return 0
+		if (price > maxPrice) {
+			return 0;
+		}
+		
+		//If product is free return max demand
+		if (price <= 0) {
+			return maxDemand;
+		}
+		
+		//else determine demand based on price
+		demand = maxDemand - Math.pow(price, 2)*maxDemand/Math.pow(maxPrice, 2);
+		
+		return demand;
+		
+	}
 }
