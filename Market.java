58e335317e116a1099ddd4b1db9dc01f5480526e
earens <49277057+earens@users.noreply.github.com>
2019-06-19 14:13:53 +0200

    Create Market.java

---
+++ Market.java
@@ -0,0 +1,15 @@
+package differentialEvolution;
+
+public class Market {
+
+	
+	
+	private double price;
+	
+	private double demand;
+	
+	public Market(double price, double demand) {
+		this.price = price;
+		this.demand = demand;
+	}
+}
