56bb8a8ce85090aa5a372dbbdc9ae8d92071295c
earens <49277057+earens@users.noreply.github.com>
2019-06-19 15:07:39 +0200

    Update Evaluation.java

--- Evaluation.java
+++ Evaluation.java
@@ -1,17 +1,39 @@
 package differentialEvolution;
 
-public class DiffenrentialEvolution {
+/**
+ * In this class we execute the Differential Evolution with all our Modules.
+ * We initialize all the Markets and Plants at the beginning and uncomment them if we
+ * don't use them in the evaluation.
+ * CSV files are also created here.
+ * @author Rosa
+ *
+ */
+public class Evaluation {
 
-	private int pop_size;
-	double[][] population;
-	
-	public DiffenrentialEvolution(int pop_size) {
+	public static void main(String[] args) {
 		
-		this.pop_size = pop_size;
-	}
-	
-	Initialization init = new Initialization(pop_size);
-	population = init.initialize();
+		// *** INITIALIZE PROBLEM ***
+		
+		// Plants
+		Plant plantA = new Plant(50000, 10000, 100);
+		Plant plantB = new Plant(600000, 80000, 50);
+		Plant plantC = new Plant(4000000, 400000, 3);
+		
+		Plant[] plants = new Plant[]{plantA, plantB, plantC};
+		
+		// Markets
+		Market market1 = new Market(0.45, 2000000);
+		Market market2 = new Market(0.25, 30000000);
+		Market market3 = new Market(0.2, 20000000);
 
 	
+		Market[] markets = new Market[]{market1, market2, market3};
+
+	
+		//parameters
+		int pop_size = 10;
+		double scale_factor = 0.5;
+		
+	}
+
 }
