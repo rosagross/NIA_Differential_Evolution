42c8bdc0e745ea520ea9c4e3b5dbe8af67ca7a2b
rosagross <rgrossmann@uni-osnabrueck.de>
2019-06-19 18:01:45 +0200

    added getter method

--- Market.java
+++ Market.java
@@ -1,34 +1,34 @@
 package differentialEvolution;
 
 public class Market {
-	
+
 	private double maxPrice;
-	
+
 	private double maxDemand;
-	
+
 	public Market(double price, double demand) {
 		this.maxPrice = price;
 		this.maxDemand = demand;
 	}
-	
+
 	public double demand(double price) {
-		
+
 		//if price is greater than max price, return 0
 		if (price > maxPrice) {
 			return 0;
 		}
-		
+
 		//If product is free return max demand
 		if (price <= 0) {
 			return maxDemand;
 		}
-		
+
 		//else determine demand based on price
 		double demandNew = maxDemand - Math.pow(price, 2)*maxDemand/Math.pow(maxPrice, 2);
-		
+
 		return demandNew;
-		
-		
+
+
 	}
 
 	/**
@@ -38,4 +38,13 @@ public class Market {
 	public double getDemand() {
 		return this.maxDemand;
 	}
+
+	/**
+	 * Getter method for the maximum price
+	 * @return
+	 */
+	public double getMaxPrice() {
+		return this.maxPrice;
+	}
+
 }
