1da74f1bbb7f8c9db18fe066f90eb20add075f94
earens <49277057+earens@users.noreply.github.com>
2019-06-19 18:31:20 +0200

    Update TrialGeneration.java

--- TrialGeneration.java
+++ TrialGeneration.java
@@ -3,48 +3,63 @@ package differentialEvolution;
 import java.util.Random;
 
 /**
- * Generate donor for crossover
+ * generate trial from donor and current population with binomial crossover
  * @author Emilia
  *
  */
-public class DonorGeneration {
+public class TrialGeneration {
+
+	/**
+	 * probability to select element from donor
+	 */
+	private double crossoverRate;
+	
+	
+	final int DIMENSIONS = 9;
 	
 	/**
-	 * set scaleFactor F
+	 * constructor for TrialGeneration
+	 * set crossoverRate
+	 * @param crossoverRate
 	 */
-	private double scaleFactor;
-	
-	/**
-	 * constructor for donor
-	 * set scaleFactor
-	 * @param scaleFactor
-	 */
-	public DonorGeneration(double scaleFactor) {
-		this.scaleFactor = scaleFactor;	
+	public TrialGeneration(double crossoverRate) {
+		this.crossoverRate = crossoverRate;
 	}
 	
+	
 	/**
-	 * generate Donor
-	 * @param whole population
-	 * @return donor
+	 * method for generating the trial
+	 * @param donor from generate donor
+	 * @param populationX from whole population
+	 * @return trial
 	 */
-	public double[] generateDonor(double[][] population) {
-		
+	public double[] generateTrial(double[] donor, double[]populationX) {
 		
 		Random random = new Random();
-		//donor
-		double[] donor = new double[9];
+		//trial
+		double[] trial = new double[DIMENSIONS];
+		//starting point
+		int r = random.nextInt(DIMENSIONS);
+		//probabilities
+		double[] c = new double[DIMENSIONS];
 		
-		//select three random vectors from population
-		double[][] selection = new double[3][9];
-		for (int i = 0; i < selection.length; i++) {
-			selection[i] = population[random.nextInt(population.length)];
+		//take element with index r(start) from donor
+		trial[r] = donor[r];
+		
+		//calculate sequence of random numbers between 0 and 1
+		for (int i = 0; i < trial.length; i++) {
+			c[i] = random.nextDouble();
 		}
 		
-		//calculate donor
-		for (int i = 0; i < 9; i++) {
-			donor[i] = selection[0][i] + scaleFactor*(selection[1][i] - selection[2][i]);
+		//generate trial with c
+		for (int i = 1; i < trial.length; i++) {
+			
+			if (c[i] < crossoverRate) {
+				trial[(r + i)%DIMENSIONS] = donor[i];
+			} else {
+				trial[(r + i)%DIMENSIONS] = populationX[i];
+			}	
 		}
-		return donor;
+		return trial;
 	}
 }
